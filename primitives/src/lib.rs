daab0485b3665828d9373e0f6f90ebde4c66ca4f
Raymond Yeh <raymond@hydrogenx.io>
2021-11-18 13:41:32 +0800

    feat: primitive scaffolding

--- primitives/src/lib.rs
+++ primitives/src/lib.rs
@@ -1,8 +1,20 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+// Primitives are used for internal representation across runtime and node
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use sp_runtime::{
+    traits::{IdentifyAccount, Verify},
+    MultiSignature,
+};
+
+pub type BlockNumber = u32;
+
+pub type Signature = MultiSignature;
+
+pub type AccountId = <<Signature as Verify>::Signer as IdentifyAccount>::AccountId;
+
+pub type Balance = u128;
+
+pub type Index = u32;
+
+pub type Hash = sp_core::H256;
