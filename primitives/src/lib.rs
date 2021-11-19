12712c0c97a2c7723a335ee78cf850f56ceceb0e
Raymond Yeh <raymond@hydrogenx.io>
2021-11-19 17:15:27 +0800

    feat: add Addres, Header as primitives

--- primitives/src/lib.rs
+++ primitives/src/lib.rs
@@ -3,7 +3,8 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use sp_runtime::{
-    traits::{IdentifyAccount, Verify},
+    generic,
+    traits::{BlakeTwo256, IdentifyAccount, Verify},
     MultiSignature,
 };
 
@@ -18,3 +19,24 @@ pub type Balance = u128;
 pub type Index = u32;
 
 pub type Hash = sp_core::H256;
+
+/// The address format for describing accounts.
+pub type Address = sp_runtime::MultiAddress<AccountId, ()>;
+
+pub type Header = generic::Header<BlockNumber, BlakeTwo256>;
+
+// time measurement based on adjustable blocktime
+// allow conversion based on runtime specified milliseconds per block
+// derived from substrate-node-template
+
+pub const fn minutes(millisec_per_block: u64) -> BlockNumber {
+    60_000 / (millisec_per_block as BlockNumber)
+}
+
+pub const fn hours(millisec_per_block: u64) -> BlockNumber {
+    minutes(millisec_per_block) * 60
+}
+
+pub const fn days(millisec_per_block: u64) -> BlockNumber {
+    hours(millisec_per_block) * 24
+}
