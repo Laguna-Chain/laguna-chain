1bd68c801fb467f684da859e1c4c03048d2094c4
Raymond Yeh <raymond@hydrogenx.io>
2021-11-19 17:19:44 +0800

    feat: add baseline consensus pallets
    frame_system and grandpa, aura, timestamp, sudo

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -7,10 +7,21 @@
 #[cfg(feature = "std")]
 include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 
-pub use frame_support::construct_runtime;
+use frame_support::{
+    construct_runtime, parameter_types,
+    traits::KeyOwnerProofSystem,
+    weights::constants::{RocksDbWeight, WEIGHT_PER_SECOND},
+};
 
-use sp_api::{impl_runtime_apis, ApisVec};
-use sp_runtime::create_runtime_str;
+use sp_api::impl_runtime_apis;
+use sp_consensus_aura::sr25519::AuthorityId as AuraId;
+use sp_runtime::{
+    app_crypto::sp_core::OpaqueMetadata,
+    create_runtime_str, generic, impl_opaque_keys,
+    traits::{AccountIdLookup, BlakeTwo256, Block as BlockT},
+    transaction_validity::{TransactionSource, TransactionValidity},
+    ApplyExtrinsicResult, KeyTypeId, Perbill,
+};
 use sp_std::prelude::*;
 
 #[cfg(feature = "std")]
@@ -19,49 +30,247 @@ use sp_version::RuntimeVersion;
 
 // TODO: include all needed pallets and their impl
 
-// TODO: import all type aliases from primives
+use pallet_grandpa::{
+    fg_primitives, AuthorityId as GrandpaId, AuthorityList as GrandpaAuthorityList,
+};
+
+pub use primitives::*;
+
+// opaque module copied from substrate-node-template, allows cli to sync the network without knowing runtime specific formats
+pub mod opaque {
+    use super::*;
+
+    pub use sp_runtime::OpaqueExtrinsic as UncheckedExtrinsic;
+
+    /// Opaque block header type.
+    pub type Header = generic::Header<BlockNumber, BlakeTwo256>;
+    /// Opaque block type.
+    pub type Block = generic::Block<Header, UncheckedExtrinsic>;
+    /// Opaque block identifier type.
+    pub type BlockId = generic::BlockId<Block>;
+
+    // TODO: construct opaque key after construct_runtime!
+    impl_opaque_keys! {
+        pub struct SessionKeys {
+            pub aura: Aura,
+            pub grandpa: Grandpa,
+        }
+    }
+}
 
 // TODO: include all needed const as well
 
 // version declaration for wasm runtime copy from substrate-node-template
-// #[sp_version::runtime_version]
-// pub const VERSION: RuntimeVersion = RuntimeVersion {
-//     spec_name: create_runtime_str!("hydro-runtime-placeholder"),
-//     impl_name: create_runtime_str!("hydro-runtime-placeholder"),
-//     authoring_version: 1,
-//     spec_version: 100,
-//     impl_version: 1,
-//     apis: RUNTIME_API_VERSIONS,
-//     transaction_version: 1,
-// };
+#[sp_version::runtime_version]
+pub const VERSION: RuntimeVersion = RuntimeVersion {
+    spec_name: create_runtime_str!("hydro-runtime-placeholder"),
+    impl_name: create_runtime_str!("hydro-runtime-placeholder"),
+    authoring_version: 1,
+    spec_version: 100, // set to 100 to help polkadot.js recognizing this custom runtime
+    impl_version: 1,
+    apis: RUNTIME_API_VERSIONS, // constructed by impl_runtime_api! macro
+    transaction_version: 1,
+};
 
 // version declaration for native runtime
-// #[cfg(feature = "std")]
-// pub fn native_version() -> NativeVersion {
-//     NativeVersion {
-//         runtime_version: VERSION,
-//         can_author_with: Default::default(),
-//     }
-// }
-
-// TODO: properly impl frame_system for Runtime
-// impl frame_system::Config for Runtime {}
-
-// TODO: type holder before we can properly construct runtime
-#[cfg(feature = "runtime-placeholder")]
-mod placeholder {
-
-    #[derive(Eq, PartialEq, Clone)]
-    pub enum Runtime {}
+#[cfg(feature = "std")]
+pub fn native_version() -> NativeVersion {
+    NativeVersion {
+        runtime_version: VERSION,
+        can_author_with: Default::default(),
+    }
 }
 
-#[cfg(feature = "runtime-placeholder")]
-use placeholder::*;
+const NORMAL_DISPATCH_RATIO: Perbill = Perbill::from_percent(75);
+
+// TODO: copied from substrate-node-template for now, plug our pallet impl later
+// define const variable for frame_system::Config
+parameter_types! {
+    pub const Version: RuntimeVersion = VERSION;
+    pub const BlockHashCount: BlockNumber = 2400;
+    /// We allow for 2 seconds of compute with a 6 second average block time.
+    pub BlockWeights: frame_system::limits::BlockWeights = frame_system::limits::BlockWeights
+    ::with_sensible_defaults(2 * WEIGHT_PER_SECOND, NORMAL_DISPATCH_RATIO);
+    pub BlockLength: frame_system::limits::BlockLength = frame_system::limits::BlockLength
+    ::max_with_normal_ratio(5 * 1024 * 1024, NORMAL_DISPATCH_RATIO);
+    pub const SS58Prefix: u8 = 42;
+}
+
+// TODO: copied from substrate-node-template for now, plug our pallet impl later
+impl frame_system::Config for Runtime {
+    /// The basic call filter to use in dispatchable.
+    type BaseCallFilter = frame_support::traits::Everything;
+    /// Block & extrinsics weights: base values and limits.
+    type BlockWeights = BlockWeights;
+    /// The maximum length of a block (in bytes).
+    type BlockLength = BlockLength;
+    /// The identifier used to distinguish between accounts.
+    type AccountId = AccountId;
+    /// The aggregated dispatch type that is available for extrinsics.
+    type Call = Call;
+    /// The lookup mechanism to get account ID from whatever is passed in dispatchers.
+    type Lookup = AccountIdLookup<AccountId, ()>;
+    /// The index type for storing how many extrinsics an account has signed.
+    type Index = Index;
+    /// The index type for blocks.
+    type BlockNumber = BlockNumber;
+    /// The type for hashing blocks and tries.
+    type Hash = Hash;
+    /// The hashing algorithm used.
+    type Hashing = BlakeTwo256;
+    /// The header type.
+    type Header = generic::Header<BlockNumber, BlakeTwo256>;
+    /// The ubiquitous event type.
+    type Event = Event;
+    /// The ubiquitous origin type.
+    type Origin = Origin;
+    /// Maximum number of block number to block hash mappings to keep (oldest pruned first).
+    type BlockHashCount = BlockHashCount;
+    /// The weight of database operations that the runtime can invoke.
+    type DbWeight = RocksDbWeight;
+    /// Version of the runtime.
+    type Version = Version;
+    /// Converts a module to the index of the module in `construct_runtime!`.
+    ///
+    /// This type is being generated by `construct_runtime!`.
+    type PalletInfo = PalletInfo;
+    /// What to do if a new account is created.
+    type OnNewAccount = ();
+    /// What to do if an account is fully reaped from the system.
+    type OnKilledAccount = ();
+    /// The data to be stored in an account.
+    type AccountData = pallet_balances::AccountData<Balance>;
+    /// Weight information for the extrinsics of this pallet.
+    type SystemWeightInfo = ();
+    /// This is used as an identifier of the chain. 42 is the generic substrate prefix.
+    type SS58Prefix = SS58Prefix;
+    /// The set code logic, just the default since we're not a parachain.
+    type OnSetCode = ();
+}
+
+pub const MILLISECS_PER_BLOCK: u64 = 6000;
+pub const SLOT_DURATION: u64 = MILLISECS_PER_BLOCK;
+
+parameter_types! {
+    pub const MinimumPeriod: u64 = SLOT_DURATION / 2;
+}
+
+impl pallet_timestamp::Config for Runtime {
+    /// A timestamp: milliseconds since the unix epoch.
+    type Moment = u64;
+    type OnTimestampSet = Aura;
+    type MinimumPeriod = MinimumPeriod;
+    type WeightInfo = ();
+}
+
+impl pallet_sudo::Config for Runtime {
+    type Event = Event;
+    type Call = Call;
+}
+
+// borderline aura and grandpa impl from substrate-node-template
+parameter_types! {
+    pub const MaxAuthorities: u32 = 32;
+}
+
+impl pallet_aura::Config for Runtime {
+    type AuthorityId = AuraId;
+    type DisabledValidators = ();
+    type MaxAuthorities = MaxAuthorities;
+}
+
+impl pallet_grandpa::Config for Runtime {
+    type Event = Event;
+    type Call = Call;
+
+    type KeyOwnerProofSystem = ();
+
+    type KeyOwnerProof =
+        <Self::KeyOwnerProofSystem as KeyOwnerProofSystem<(KeyTypeId, GrandpaId)>>::Proof;
+
+    type KeyOwnerIdentification = <Self::KeyOwnerProofSystem as KeyOwnerProofSystem<(
+        KeyTypeId,
+        GrandpaId,
+    )>>::IdentificationTuple;
+
+    type HandleEquivocation = ();
+
+    type WeightInfo = ();
+    type MaxAuthorities = MaxAuthorities;
+}
 
 // runtime as enum, can cross reference enum variants as pallet impl type associates
-// construct_runtime!(
-//     pub enum Runtime {}
-// );
+// this macro also mixed type to all pallets so that they can adapt through a shared type
+// be cautious that compile error arise if the pallet and construct_runtime can't be build at the same time, most of the time they cross reference each other
+construct_runtime!(
+    pub enum Runtime
+    where
+        Block = Block,
+        NodeBlock = opaque::Block,
+        UncheckedExtrinsic = UncheckedExtrinsic
+        {
+            System: frame_system,
+            Timestamp: pallet_timestamp,
+            Aura: pallet_aura,
+            Grandpa: pallet_grandpa,
+            Sudo: pallet_sudo,
+        }
+);
 
-// expose runtime apis
-// impl_runtime_apis! {}
+// The following types are copied from substrate-node-template to boostrap development
+
+/// Block type as expected by this runtime.
+pub type Block = generic::Block<Header, UncheckedExtrinsic>;
+
+/// The SignedExtension to the basic transaction logic
+pub type SignedExtra = (
+    frame_system::CheckSpecVersion<Runtime>,
+    frame_system::CheckTxVersion<Runtime>,
+    frame_system::CheckGenesis<Runtime>,
+    frame_system::CheckEra<Runtime>,
+    frame_system::CheckNonce<Runtime>,
+    frame_system::CheckWeight<Runtime>,
+    // pallet_transaction_payment::ChargeTransactionPayment<Runtime>,
+);
+/// Unchecked extrinsic type as expected by this runtime.
+pub type UncheckedExtrinsic = generic::UncheckedExtrinsic<Address, Call, Signature, SignedExtra>;
+
+/// Executive: handles dispatch to the various modules -> virtual dispatch caller
+pub type Executive = frame_executive::Executive<
+    Runtime,
+    Block,
+    frame_system::ChainContext<Runtime>,
+    Runtime,
+    AllPallets,
+    (), // TODO: properly enable runtime upgrade later
+>;
+
+// expose runtime apis,
+// TODO: common api impl are derived from substrate-node-template, add custom runtime-api later
+impl_runtime_apis! {
+    impl sp_api::Core<Block> for Runtime {
+        fn version() -> RuntimeVersion {
+            VERSION
+        }
+
+        fn execute_block(block: Block) {
+            Executive::execute_block(block);
+        }
+
+        fn initialize_block(header: &<Block as BlockT>::Header) {
+            Executive::initialize_block(header)
+        }
+    }
+
+
+    impl sp_api::Metadata<Block> for Runtime {
+        fn metadata() -> OpaqueMetadata {
+            OpaqueMetadata::new(Runtime::metadata().into())
+        }
+    }
+
+    // TODO: add other needed runtime-api
+
+    // TODO: enable runtime-benchmark api for auto weight suggestion
+
+}
