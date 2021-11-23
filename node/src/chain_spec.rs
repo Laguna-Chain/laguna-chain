6d6d36b0d53c98ebaa231f9ebdde1efb32caacbf
Raymond Yeh <raymond@hydrogenx.io>
2021-11-23 14:39:59 +0800

    feat: init testnet chainspec setup

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -2,3 +2,5 @@ pub mod testnet;
 
 // TODO: placeholder for now.
 pub mod mainnet;
+
+pub mod util;
---
+++ node/src/chain_spec/util.rs
@@ -0,0 +1,24 @@
+use primitives::{AccountId, AccountPublic, IdentifyAccount};
+use sp_core::{Pair, Public};
+use sp_consensus_aura::sr25519::AuthorityId as AuraId;
+use sp_finality_grandpa::AuthorityId as GrandpaId;
+
+/// Generate a crypto pair from seed.
+fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Public {
+    TPublic::Pair::from_string(&format!("//{}", seed), None)
+        .expect("static values are valid; qed")
+        .public()
+}
+
+/// Generate an account ID from seed.
+pub fn get_account_id_from_seed<TPublic: Public>(seed: &str) -> AccountId
+where
+    AccountPublic: From<<TPublic::Pair as Pair>::Public>,
+{
+    AccountPublic::from(get_from_seed::<TPublic>(seed)).into_account()
+}
+
+/// Generate an Aura authority key.
+pub fn authority_keys_from_seed(s: &str) -> (AuraId, GrandpaId) {
+    (get_from_seed::<AuraId>(s), get_from_seed::<GrandpaId>(s))
+}
