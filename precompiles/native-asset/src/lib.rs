28035744a8e5ffe1b56ec77550809b2d2c1bde89
Raymond Yeh <raymond@hydrogenx.io>
2022-01-27 18:55:03 +0800

    fix: construct error under no_std

--- precompiles/native-asset/src/lib.rs
+++ precompiles/native-asset/src/lib.rs
@@ -130,7 +130,7 @@ where
 			amount.into(),
 			frame_support::traits::ExistenceRequirement::AllowDeath,
 		)
-		.map_err(|e| ExitError::Other(format!("{:?}", e).into()))?;
+		.map_err(|e| ExitError::Other("UNABLE_TO_WITHDRAW"))?;
 
 		gasometer.record_cost(RuntimeHelper::<Runtime>::db_read_gas_cost())?;
 
