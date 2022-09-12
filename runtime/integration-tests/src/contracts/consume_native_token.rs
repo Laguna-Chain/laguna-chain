e533def06383ea036e832e2710c3668c47df46c5
realnimish <realnimish@gmail.com>
2022-09-12 19:37:27 +0530

    Minor change

--- runtime/integration-tests/src/contracts/consume_native_token.rs
+++ runtime/integration-tests/src/contracts/consume_native_token.rs
@@ -66,7 +66,7 @@ mod tests {
 			.rev()
 			.find_map(|r| {
 				if let Event::Contracts(pallet_contracts::Event::Instantiated {
-					deployer,
+					deployer: _,
 					contract,
 				}) = &r.event
 				{
