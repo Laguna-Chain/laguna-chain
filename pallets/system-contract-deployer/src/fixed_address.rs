07f4b6d5e60959dd8bff931470bc340d03a6ac37
Raymond Yeh <extraymond@gmail.com>
2022-11-15 09:02:06 +0000

    refactor: remove unused bits
    Signed-off-by: Raymond Yeh <extraymond@gmail.com>

--- pallets/system-contract-deployer/src/fixed_address.rs
+++ pallets/system-contract-deployer/src/fixed_address.rs
@@ -4,43 +4,10 @@
 
 use core::marker::PhantomData;
 
-use frame_support::{pallet_prelude::Decode, sp_runtime, traits::Get};
-use pallet_contracts::{AddressGenerator, DefaultAddressGenerator};
-use sp_core::{crypto::UncheckedFrom, ByteArray};
-use sp_runtime::{traits::AccountIdConversion, AccountId32};
-
-// pub struct CustomAddressGenerator;
-
-// impl<T> AddressGenerator<T> for CustomAddressGenerator
-// where
-// 	T: crate::Config,
-// 	T::AccountId: UncheckedFrom<T::Hash> + AsRef<[u8]>,
-// {
-// 	fn generate_address(
-// 		deploying_address: &T::AccountId,
-// 		code_hash: &T::Hash,
-// 		salt: &[u8],
-// 	) -> T::AccountId {
-// 		// Retrieving the pallet_system_contract_deployer AccountId
-// 		let key = <T as crate::Config>::PalletId::get()
-// 			.try_into_account()
-// 			.expect("Invalid PalletId");
-
-// 		if deploying_address == &key {
-// 			// Decoding the salt to the destined deployment contract address
-// 			let destined_addr: [u8; 32] = salt.try_into().unwrap();
-// 			let contract_addr = AccountId32::from(destined_addr);
-// 			return T::AccountId::decode(&mut contract_addr.as_ref())
-// 				.expect("Cannot create an AccountId from the given salt")
-// 		}
-
-// 		<DefaultAddressGenerator as AddressGenerator<T>>::generate_address(
-// 			deploying_address,
-// 			code_hash,
-// 			salt,
-// 		)
-// 	}
-// }
+use frame_support::{sp_runtime, traits::Get};
+use pallet_contracts::AddressGenerator;
+use sp_core::ByteArray;
+use sp_runtime::traits::AccountIdConversion;
 
 /// generate fixed-address if the deployer is the system-deployer, otherwise use the specified
 /// address generator
